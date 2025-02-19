d128230bd549432002d23c85dffcf0ae01aae907
Yuuki-main <rajatmalviya1738@gmail.com>
2025-02-19 22:30:15 +0530

    first commit 121

---
+++ config.ts
@@ -0,0 +1,8 @@
+const config = {
+  domainName: "www.bountygame25.in",
+  appName: "BountyGame",
+  appDescription:
+    "Bounty game is one of the most rewarding platform",
+};
+
+export default config;
